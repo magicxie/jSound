043be4911e34835785cf5540c2dd29650d1c9a18
magic.xie <eggcanfly@gmail.com>
2013-09-04 18:09:18 +0800

    Update jquery.jsound.js

--- jquery.jsound.js
+++ jquery.jsound.js
@@ -3,90 +3,119 @@
  * 
  * jQuery JSound Version 1.0
  *
- * based on jquery.sound.js by Jörn Zaefferer
+ * based on jquery.sound.js by JÃ¶rn Zaefferer
  * Ramakrishna Anand.J
  * Licensed under the GNU GENERAL PUBLIC LICENSE:
  * http://opensource.org/licenses/GPL-3.0
- *   
- * $Id$
  */
 /**
  * API Documentation
  * 
  * // play a sound from the url
- * $.sound.play(url)
+ * var sound = $.sound(url);
+ * sound.play();
  * 
- * // play a sound from the url, on a track, stopping any sound already running on that track
- * $.sound.play(url, {
- *   track: "track1"
- * });
  * 
  * // mute a sound 
- * $.sound.mute();
+ * sound.mute();
  * 
  * // unmute a sound
- * $.sound.unmute();
+ * sound.unmute();
  * 
- * // disable playing sounds
- * $.sound.enabled = false;
- * 
- * // enable playing sounds
- * $.sound.enabled = true
  */
 
 (function($) {
-var volume;
-var bgSound;
-$.sound = {
-        tracks: {},
-        enabled: true,
-        template: function(src) {
-			      return '<audio id="bgsoundplayer" style="height:0" loop="false" autoplay="true"><source src="'+ src +'"  ></audio>';
-        },
-		mute: function(level){
-		if($.browser.msie){ level=-10000; }else{ level=0.0; }
-		bgSound = document.getElementById("bgsoundplayer");
-        bgSound.volume=level;
-		},
-		unmute: function(){
-		if($.browser.msie){ level=0; }else{ level=1.0; }
-		bgSound = document.getElementById("bgsoundplayer");
-        bgSound.volume=level;
-		},
-		volume:function(level){
-		bgSound = document.getElementById("bgsoundplayer");
-        bgSound.volume=level;
-		},
-        play: function(url, options,volume){
-                if (!this.enabled)
-                        return;
-                var settings = $.extend({
-                        url: url
-                }, options);
+
+    var Sound = function(url, options){
+      
+        this.url = url;
+        this.options = options;
+        /*$.support.leadingWhitespace IE6-8*/
+        this.wrapElement = (!$.support.leadingWhitespace) ? $('<bgsound id="s" volume="10000" />').attr({
+                            src: url + '.wav',
+                            loop: 1,
+						    autostart: true
+                          })
+                            : $('<audio style="height:0" ><source src="'+ url +'.wav" type="audio/wave" /><source src="'+ url +'.mp3" type="audio/mp3" /><source src="'+ url +'.ogg" type="audio/ogg" /></audio>');                          
+        
+        if(typeof Sound._init == 'undefined'){
                 
-                if (settings.track) {
-                        if (this.tracks[settings.track]) {
-                                var current = this.tracks[settings.track];
-                                // TODO check when Stop is avaiable, certainly not on a jQuery object
-                                current.Stop && current.Stop();
-                                current.remove();  
-                        }
+            /*mute*/    
+            var _mute = false;    
+            
+            $.sound.mute = Sound.prototype.mute = (function(){
+                return function(){_mute = true; return _mute;}
+            })();
+            
+            $.sound.unmute = Sound.prototype.unmute = (function(){
+                return function(){_mute = false; return _mute;}
+            })();
+            
+            $.sound.toogleMute = Sound.prototype.toogleMute = (function(){
+                return function(){_mute = !_mute; return _mute;}
+            })();
+            
+            $.sound.isMute = Sound.prototype.isMute = (function(){
+                return function(){return _mute;}
+            })();
+                
+            /*sound cache*/
+            var _cache = {};
+            Sound.prototype.cache = (function(){          
+                return function(){
+                    return _cache;
+                };
+            })();    
+                       
+            
+            Sound.prototype.playIE = function(){
+              
+              var src = this.wrapElement.attr('src');
+              this.wrapElement.attr('src', '');
+              this.wrapElement.attr('src', src);
+            }
+            
+            Sound.prototype.playH5 = function(){
+                this.wrapElement[0].play();
+            }
+            
+             /*play*/
+            Sound.prototype.play = function(url){
+
+                if(url === undefined){
+                    
+                }else{
+                    this.url = url;
                 }
                 
-                var element = $.browser.msie
-                        ? $('<bgsound id="bgsoundplayer"  volume="'+volume+'" />').attr({
-                        src: settings.url,
-                        loop: 1,
-						autostart: true
-                      })
-                        : $(this.template(settings.url));
-                        
-                element.appendTo("body");
+                if(!this.cache()[this.url]){
+                  this.wrapElement.appendTo('body');
+                  //console.log(this.cache())
+                  this.cache()[this.url] = true;
+                }
                 
-                if (settings.track) {
-                        this.tracks[settings.track] = element;
-                }               
-                return element;
+                if(!_mute){          
+                 
+                  
+                 if((!$.support.leadingWhitespace)){
+                    this.playIE();
+                 }else{
+                    this.playH5();
+                 }
+                 
+                }                       
+            }
+            
+            //end init
+            Sound._init = true;        
         }
-};
+
+        //return this;
+        
+    }
+
+    $.sound = function(url){
+        return new Sound(url);
+    }
+    
 })(jQuery);
